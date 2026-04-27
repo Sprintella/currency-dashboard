10bf9dc0577be38726c26e3dee72bafb1b3f651a
Sprintella <148622407+Sprintella@users.noreply.github.com>
2026-04-27 20:22:17 +0200

    feat: Dodanie testów jednostkowych do frontendu

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,23 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+/// <reference types="jasmine" />
+
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { App } from './app';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('Powinien wywołać funkcję pobierania po kliknięciu przycisku (NBP)', () => {
+    spyOn(component, 'fetchFromNBP');
+
+    const buttonDebug = fixture.debugElement.query(By.css('.btn-primary'));
+    buttonDebug.triggerEventHandler('click', null);
+
+    expect(component.fetchFromNBP).toHaveBeenCalledWith(1); // Sprawdzenie, czy funkcja została wywołana z parametrem 1
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+  it('Powinien poprawnie wyświetlać dane w tabeli', () => {
+    // Podstawienie sztucznych danych, aby wymusić na Angularze narysowanie tabeli
+    component.filteredRates = [
+      { id: 1, effective_date: '2026-04-24', code: 'USD', currency: 'dolar amerykanski', mid: 4.05 }
+    ];
+
+    fixture.detectChanges(); // Wymuszenie przerywania widoku HTML
+
+    const tableRows = fixture.debugElement.queryAll(By.css('tbody tr'));
+    expect(tableRows.length).toBe(1); // Tabela powinna mieć dokładnie 1 wiersz
+
+    // Wyciągnięcie komórek z pierwszego wiersza i sprawdzenie ich zawartości
+    const rowCells = tableRows[0].queryAll(By.css('td'));
+    expect(rowCells[0].nativeElement.textContent.trim()).toBe('2026-04-24');
+    expect(rowCells[1].nativeElement.textContent.trim()).toBe('USD');
   });
 });
